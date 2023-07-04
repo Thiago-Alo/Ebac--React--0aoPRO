73f1f97046c5ee482fada39bd64191d7525ed5db
Thiago Alô <78623134+Thiago-Alo@users.noreply.github.com>
2023-07-04 23:35:49 +0100

    modulo 22- ok

--- Módulo 22 - Projeto Final Módulo 22/src/App.tsx
+++ Módulo 22 - Projeto Final Módulo 22/src/App.tsx
@@ -1,31 +1,11 @@
-import { Provider } from 'react-redux'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import EstiloGlobal, { Container } from './styles'
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import TodoList from './containers/TodoList';
+import './styles.scss';
 
-import store from './store'
-import Home from './pages/Home'
-import Cadastro from './pages/Cadastro'
-
-const rotas = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/novo',
-    element: <Cadastro />
-  }
-])
-
-function App() {
-  return (
-    <Provider store={store}>
-      <EstiloGlobal />
-      <Container>
-        <RouterProvider router={rotas} />
-      </Container>
-    </Provider>
-  )
-}
-
-export default App
+export default () => (
+  <Provider store={store}>
+    <TodoList />
+  </Provider>
+);
